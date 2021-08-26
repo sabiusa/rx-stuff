0349476f709913c72a5da8cbd525257c8f370ba9
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-26 13:17:51 +0400

    Add basic observe/subscribe examples

--- observables/observables-test/observables_test.swift
+++ observables/observables-test/observables_test.swift
@@ -10,21 +10,63 @@ import RxSwift
 
 class observables_test: XCTestCase {
 
-    func test_something() {
+    func test_rx() {
         let a = 1
         
-        test_observables()
+        test_empty()
+        test_never()
+        test_singleValue()
         
         XCTAssertEqual(a, 1)
     }
     
-    func test_observables() {
+    func test_empty() {
+        example(of: "empty") {
+            let obs = Observable<Void>.empty()
+            
+            obs.subscribe(
+                onNext: { elem in
+                    print(elem)
+                },
+                onCompleted: {
+                    print("complete")
+                }
+            )
+        }
+    }
+    
+    func test_never() {
+        example(of: "never") {
+            let obs = Observable<Void>.never()
+            
+            obs.subscribe(
+                onNext: { elem in
+                    print(elem)
+                },
+                onError: { error in
+                    print(error)
+                },
+                onCompleted: {
+                    print("complete")
+                }
+            )
+        }
+    }
+    
+    func test_singleValue() {
         example(of: "just, of, from") {
-          let one = 1
-          let two = 2
-          let three = 3
+            let one = 1
+            let two = 2
+            let three = 3
           
-          let observable = Observable<Int>.just(one)
+            let obs = Observable.of(one, two, three)
+            obs.subscribe { event in
+                if let elem = event.element {
+                    print(elem)
+                } else if event.isCompleted {
+                    print("complete")
+                }
+            }
         }
     }
 
