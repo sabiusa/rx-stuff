159b91dbdfc4ef0f32cddacaf72b606f33a9152f
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-30 15:12:52 +0400

    Add initial setup of

---
+++ rxstagram/rxstagram/Helpers/PhotoWriter.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoWriter.swift
+//  rxstagram
+//
+//  Created by Saba Khutsishvili on 30.08.21.
+//
+
+import Foundation
+import UIKit
+import Photos
+
+class PhotoWriter {
+    
+    enum Errors: Error {
+        case couldNotSavePhoto
+    }
+    
+}
