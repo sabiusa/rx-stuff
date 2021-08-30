e3fdb6681163a55f4d3f24d780700d7be66689e5
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-30 13:05:16 +0400

    Add example of `BehaviorRelay`

--- relays/relays-tests/relays_tests.swift
+++ relays/relays-tests/relays_tests.swift
@@ -27,5 +27,31 @@ class relays_tests: XCTestCase {
             relay.accept("Who dis")
         }
     }
+    
+    func test_behaviorRelay() {
+        example(of: "BehaviorRelay") {
+            let bag = DisposeBag()
+            
+            let relay = BehaviorRelay(value: "Initial value")
+            relay.accept("New value")
+            
+            relay
+                .subscribe(onNext: {
+                    print("1) \($0)")
+                })
+                .disposed(by: bag)
+            
+            relay.accept("2")
+            
+            relay.subscribe(onNext: {
+                print("2) \($0)")
+            })
+            .disposed(by: bag)
+            
+            relay.accept("3")
+            
+            print(relay.value)
+        }
+    }
 
 }
