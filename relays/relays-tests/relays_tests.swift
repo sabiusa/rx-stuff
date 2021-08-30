a8c09e6f7a57e38112f46137ad9c0a6c168fd6bc
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-30 12:55:40 +0400

    Add example of `PublishRelay`

--- relays/relays-tests/relays_tests.swift
+++ relays/relays-tests/relays_tests.swift
@@ -6,13 +6,26 @@
 //
 
 import XCTest
+import RxSwift
+import RxRelay
 
 class relays_tests: XCTestCase {
 
-    func test() {
-        let a = 1
-        
-        XCTAssertEqual(a, 1)
+    func test_publishRelay() {
+        example(of: "PublishRelay") {
+            let bag = DisposeBag()
+            
+            let relay = PublishRelay<String>()
+            relay.accept("Knock knock")
+            
+            relay
+                .subscribe(onNext: {
+                    print($0)
+                })
+                .disposed(by: bag)
+            
+            relay.accept("Who dis")
+        }
     }
 
 }
