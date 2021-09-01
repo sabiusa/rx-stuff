3da9a6a1c5c53a1c614897a8b44f01d64f4c0e88
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-01 15:11:53 +0400

    Add scheduler testing app

---
+++ schedulers/Pods/Target Support Files/Pods-schedulers/Pods-schedulers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_schedulers : NSObject
+@end
+@implementation PodsDummy_Pods_schedulers
+@end
---
+++ schedulers/schedulers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  schedulers
+//
+//  Created by Saba Khutsishvili on 01.09.21.
+//
+
+import Foundation
+import RxSwift
+
+print("Hello, World!")
+
