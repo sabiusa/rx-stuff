067f5eb10a32bda75b66bb72ff7a4a7a151fc039
Saba Khutsishvili <sabakhuts@gmail.com>
2021-09-01 15:54:47 +0400

    Add example of different schedulers

--- schedulers/schedulers/main.swift
+++ schedulers/schedulers/main.swift
@@ -15,12 +15,25 @@ let bag = DisposeBag()
 let animal = BehaviorSubject(value: "[dog]")
 
 animal
-  .dump()
-  .dumpingSubscription()
-  .disposed(by: bag)
+    .dump()
+    .dumpingSubscription()
+    .disposed(by: bag)
 
-// Start coding here
+let fruit = Observable<String>.create { observer in
+    observer.onNext("[apple]")
+    sleep(2)
+    observer.onNext("[pineapple]")
+    sleep(2)
+    observer.onNext("[strawberry]")
+    return Disposables.create()
+}
 
+fruit
+    .subscribeOn(globalScheduler)
+    .dump()
+    .observeOn(MainScheduler.instance)
+    .dumpingSubscription()
+    .disposed(by: bag)
 
 RunLoop.main.run(until: Date(timeIntervalSinceNow: 13))
 
