1acfeddefa093c1e1f18a6f00d0e44cdebb09ef1
Saba Khutsishvili <saba.khutsishvili@space.ge>
2021-09-02 13:11:13 +0400

    Add testing sample iOS app

---
+++ rx-test/rx-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rx-test
+//
+//  Created by Saba Khutsishvili on 02.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
